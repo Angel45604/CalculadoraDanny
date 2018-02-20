a50698c25e7946298e589d4029de54c12e9f6268
Angel Marcos <angel.mauricio.marcos@live.com>
2018-02-19 18:35:50 -0600

    Yo digo que ahora si funciona

--- app/src/main/java/com/quintana/daniela/calculator/MainActivity.java
+++ app/src/main/java/com/quintana/daniela/calculator/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import java.util.ArrayList;
+import java.util.Stack;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    String tmp = "";
-    String op = "";
+    //Evalua el caracter a imprimir en pantalla y lo imprime
     public void printChar(View v) {
         Button b = (Button) v;
         String a = b.getText().toString();
 
         TextInputEditText txt = (TextInputEditText) findViewById(R.id.txtNum);
+        TextInputEditText res = (TextInputEditText) findViewById(R.id.txtRes);
 
         if(a.equals("")) {
             txt.setText("0");
@@ -36,73 +39,46 @@ public class MainActivity extends AppCompatActivity {
             Log.d("BTN", txt.getText().toString().substring(0,txt.getText().toString().length()-1));
             txt.setText(txt.getText().toString().substring(0,txt.getText().toString().length()-1));
         } else if(a.equals("+")) {
-            if(tmp.equals("")) {
-                tmp = txt.getText().toString();
-                txt.setText("0");
-                op = "+";
-            } else {
-                int res = Integer.parseInt(tmp) + Integer.parseInt(txt.getText().toString());
-                tmp = "";
-                op = "";
-                txt.setText("" + res);
-            }
-        }else if(a.equals("-")) {
-            if(tmp.equals("")) {
-                tmp = txt.getText().toString();
-                txt.setText("0");
-                op = "-";
-            } else {
-                int res = Integer.parseInt(tmp) - Integer.parseInt(txt.getText().toString());
-                tmp = "";
-                op = "";
-                txt.setText(""+res);
-            }
-        }else if(a.equals("X")) {
-            if(tmp.equals("")) {
-                tmp = txt.getText().toString();
-                txt.setText("0");
-                op = "*";
-            } else {
-                int res = Integer.parseInt(tmp) * Integer.parseInt(txt.getText().toString());
-                tmp = "";
-                op = "";
-                txt.setText(""+res);
-            }
-        }else if(a.equals("/")) {
-            if(tmp.equals("")) {
-                tmp = txt.getText().toString();
-                txt.setText("0");
-                op = "/";
-            } else {
-                int res = Integer.parseInt(tmp) / Integer.parseInt(txt.getText().toString());
-                tmp = "";
-                op = "";
-                txt.setText(""+res);
-            }
-        } else if (a.equals("=")) {
-            if (op.equals("")) {
+            if(txt.getText().toString().equals("")) {
                 return;
-            }else if (op.equals("+")) {
-                int res = Integer.parseInt(tmp) + Integer.parseInt(txt.getText().toString());
-                txt.setText(""+res);
-                tmp="";
-                op="";
-            }else if (op.equals("-")) {
-                int res = Integer.parseInt(tmp) - Integer.parseInt(txt.getText().toString());
-                txt.setText(""+res);
-                tmp="";
-                op="";
-            }else if (op.equals("X")) {
-                int res = Integer.parseInt(tmp) * Integer.parseInt(txt.getText().toString());
-                txt.setText(""+res);
-                tmp="";
-                op="";
-            }else if (op.equals("/")) {
-                int res = Integer.parseInt(tmp) / Integer.parseInt(txt.getText().toString());
-                txt.setText(""+res);
-                tmp="";
-                op="";
             }
+            txt.setText(txt.getText().toString()+"+");
+
+        }else if(a.equals("-")) {
+
+            if(txt.getText().toString().equals("")) {
+                return;
+            }
+            txt.setText(txt.getText().toString()+"-");
+
+        }else if(a.equals("X")) {
+
+            if(txt.getText().toString().equals("")) {
+                return;
+            }
+            txt.setText(txt.getText().toString()+"*");
+
+        }else if(a.equals("/")) {
+
+            if(txt.getText().toString().equals("")) {
+                return;
+            }
+            txt.setText(txt.getText().toString()+"/");
+
+        }else if(a.equals("(")) {
+
+            txt.setText(txt.getText().toString()+"(");
+
+        }else if(a.equals(")")) {
+
+            txt.setText(txt.getText().toString()+")");
+
+        } else if (a.equals("=")) {
+
+            String mathExpr = infijoAPostfijo(txt.getText().toString());
+            Log.d("RES", ""+funcionEvaluaPostFijo(mathExpr));
+            res.setText(""+funcionEvaluaPostFijo(mathExpr));
+
         }else {
             if(txt.getText().toString().equals("0")) {
                 txt.setText(a);
@@ -111,8 +87,179 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
 
-        Log.d("BTN", tmp);
+    /**
+     * @param op
+     * @return Jerarquia de operaciones
+     */
+    private static int pref(String op) {
+        int prf = 99;
+        if (op.equals("^")) prf = 5;
+        if (op.equals("*") || op.equals("/")) prf = 4;
+        if (op.equals("+") || op.equals("-")) prf = 3;
+        if (op.equals(")")) prf = 2;
+        if (op.equals("(")) prf = 1;
+        return prf;
+    }
 
+    /**
+     * @param s
+     * @return Elimina espacios en blanco e inserta los operadores
+     */
+    private static String depurar(String s) {
+        s = s.replaceAll("\\s+", ""); //Elimina espacios en blanco
+        s = "(" + s + ")";
+        String simbols = "+-*/()^";
+        String str = "";
+
+        //Deja espacios entre operadores
+        for (int i = 0; i < s.length(); i++) {
+            if (simbols.contains("" + s.charAt(i))) {
+                str += " " + s.charAt(i) + " ";
+            }else str += s.charAt(i);
+        }
+        return str.replaceAll("\\s+", " ").trim();
+    }
+
+    /**
+     * @param expresion
+     * @return Convierte la expresion infija a postfija
+     */
+    public String infijoAPostfijo(String expresion){
+        //Depurar la expresion algebraica
+        String expr = depurar(expresion);
+        String[] arrayInfix = expr.split(" ");
+        String literales = "x";
+
+        //Declaración de las pilas
+        Stack < String > E = new Stack < String > (); //Pila entrada
+        Stack < String > P = new Stack < String > (); //Pila temporal para operadores
+        Stack < String > S = new Stack < String > (); //Pila salida
+
+        //Añadir la array a la Pila de entrada (E)
+        for (int i = arrayInfix.length - 1; i >= 0; i--) {
+            E.push(arrayInfix[i]);
+        }
+
+        try {
+            //Infijo a Postfijo
+            while (!E.isEmpty()) {
+                switch (pref(E.peek())){
+                    case 1:
+                        P.push(E.pop());
+                        break;
+                    case 3:
+                    case 4:
+                        while(pref(P.peek()) >= pref(E.peek())) {
+                            S.push(P.pop());
+                        }
+                        P.push(E.pop());
+                        break;
+                    case 2:
+                        while(!P.peek().equals("(")) {
+                            S.push(P.pop());
+                        }
+                        P.pop();
+                        E.pop();
+                        break;
+                    case 5:
+                        while(pref(P.peek()) >pref(E.peek())){
+                            S.push(P.pop());
+                        }
+                        P.push(E.pop());
+                        break;
+                    default:
+                        if(literales.contains(E.peek().toLowerCase())){
+                            S.push(E.pop().toLowerCase());
+                        }
+                        else{
+                            S.push(E.pop());
+                        }
+                }
+            }
+
+            //Eliminacion de `impurezas´ en la expresiones algebraicas
+            String infix = expr.replace(" ", "");
+            String postfix = S.toString().replaceAll("[\\]\\[,]", "");
+
+            //Mostrar resultados:
+            System.out.println("Expresion Infija: " + infix);
+            System.out.println("Expresion Postfija: " + postfix);
+            return postfix;
+
+        }catch(Exception ex){
+            System.out.println("Error en la expresión algebraica");
+            System.err.println(ex);
+        }
+        return null;
+    }
+
+    /**
+     * @param expPostFijo
+     * @return Evalua la expreion postfijo
+     */
+    private Double funcionEvaluaPostFijo(String expPostFijo)
+    {
+        String[] post = expPostFijo.split(" ");
+
+        // Declaración de las pilas
+        Stack<String> E = new Stack<String>(); // Pila entrada
+        Stack<String> P = new Stack<String>(); // Pila de operandos
+
+        // Añadir post (array) a la Pila de entrada (E)
+        for (int i = post.length - 1; i >= 0; i--) {
+            E.push(post[i]);
+        }
+
+        String operadores = "+-*/^";
+        while (!E.isEmpty()) {
+            if (operadores.contains("" + E.peek()))
+            {
+                Double resParcial = evaluar(E.pop(), P.pop(), P.pop());
+                if(resParcial != null)
+                    P.push( resParcial + "");
+                else
+                    return null;
+            }
+            else
+            {
+                P.push(E.pop());
+            }
+        }
+        Double resultado = Double.parseDouble(P.pop());
+        if(P.isEmpty()){
+            return resultado;
+        }
+        return null;
+    }
+
+    /**
+     * @param op
+     * @param n2
+     * @param n1
+     * @return Evalua los operadores.
+     */
+    private static Double evaluar(String op, String n2, String n1) {
+        try
+        {
+            double num1 = Double.parseDouble(n1);
+            double num2 = Double.parseDouble(n2);
+            if (op.equals("+"))
+                return (num1 + num2);
+            if (op.equals("-"))
+                return (num1 - num2);
+            if (op.equals("*"))
+                return (num1 * num2);
+            if (op.equals("/"))
+                return (num1 / num2);
+            if (op.equals("^"))
+                return (Math.pow(num1, num2));
+            return 0d;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
     }
 }
